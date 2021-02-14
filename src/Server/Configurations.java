d30025a01673d513f7744aeba4a62810d525819b
amit yosefi <amit.yosefi@gmail.com>
2021-02-14 14:22:31 +0200

    final project

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -11,48 +11,10 @@ public class Configurations {
 
     public static void configurationsInit() {
 
-        try (OutputStream output = new FileOutputStream("Resources/config.properties")) {
 
-            Properties prop = new Properties();
-
-            // set the config.properties value
-            prop.setProperty("Generate", "MyMazeGenerator");
-            prop.setProperty("Search", "BreadthFirstSearch");
-            prop.setProperty("ThreadPool", String.valueOf(5));
-
-            // save config.properties to project root folder
-            prop.store(output, null);
-
-        } catch (IOException io) {
-            io.printStackTrace();
-        }
     }
 
-    /*
-        public static String loadProperty(String str)
-        {
-            String propertyValue = "";
 
-            try (InputStream input = Configurations.class.getClassLoader().getResourceAsStream("Resources/config.properties"))
-            {
-                if (input == null) {
-                    System.out.println("Sorry, unable to find config.properties");
-                    return null;
-                }
-
-                //load a config.properties file from class path, inside static method
-                Properties prop = new Properties();
-                prop.load(input);
-                propertyValue = prop.getProperty(str);
-
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-
-            return propertyValue;
-        }
-
-     */
     public static String loadProperty(String str) {
         Properties prop = new Properties();
 
